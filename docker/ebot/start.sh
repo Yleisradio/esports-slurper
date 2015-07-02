e0ef8cb7aa2e81d43a4d9cf7e374c36080626b24
Jussi Pöri <jussi.pori@gmail.com>
2015-07-02 20:17:15 +0300

    Fixed

--- docker/ebot/start.sh
+++ docker/ebot/start.sh
@@ -4,7 +4,7 @@ set -o errexit
 main () {
 
     script_path=$(pwd):/data/
-    docker run -d --name esports-mysql  -v $script_path  -e MYSQL_ROOT_PASSWORD=admin -e MYSQL_DATABASE=ebotv3 -e MYSQL_USER=ebotv3 -e MYSQL_PASSWORD=ebotv3 mysql/mysql-server:latest
+    docker run -d --name esports-mysql  -v $script_path -p 3307:3306  -e MYSQL_ROOT_PASSWORD=admin -e MYSQL_DATABASE=ebotv3 -e MYSQL_USER=ebotv3 -e MYSQL_PASSWORD=ebotv3 mysql/mysql-server:latest
 
 }
 
